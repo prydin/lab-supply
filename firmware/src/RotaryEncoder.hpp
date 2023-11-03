9d66df7e3eb1547be9ece3d6d901bc5df96801e4
prydin <prydin@vmware.com>
2023-11-03 11:27:41 -0400

    Cleanup

--- firmware/src/RotaryEncoder.hpp
+++ firmware/src/RotaryEncoder.hpp
@@ -1,3 +1,25 @@
+/*
+Copyright 2023, Pontus Rydin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 #include <Arduino.h>
 
 class RotaryEncoder
@@ -6,12 +28,15 @@ public:
     RotaryEncoder(int8_t phase1, int8_t phase2, int8_t sw, int16_t min, int16_t max, int16_t fine, int16_t coarse, bool reversed = false)
         : m_phase1(phase1),
           m_phase2(phase2),
-          m_fine(fine),
-          m_coarse(coarse),
+          m_sw(sw),
           m_max(max * 2),
           m_min(min * 2),
-          m_sw(sw),
-          m_reversed(reversed)
+          m_fine(fine),
+          m_coarse(coarse),
+          m_reversed(reversed),
+          m_change(0),
+          m_previous_state(0),
+          m_steps(0)
     {
         pinMode(phase1, INPUT_PULLUP);
         pinMode(phase2, INPUT_PULLUP);
@@ -84,15 +109,15 @@ public:
     }
 
 private:
-    int8_t m_phase1 = 0;
-    int8_t m_phase2 = 0;
-    int8_t m_sw = 0;
-    float m_coarse = 1.0;
-    float m_fine = 1.0;
-    bool m_reversed = false;
-    volatile int m_change = 0;
-    int8_t m_previous_state = 0;
-    int m_steps = 0;
-    int16_t m_max = 0;
-    int16_t m_min = 0;
+    const int8_t m_phase1;
+    const int8_t m_phase2;
+    const int8_t m_sw;
+    const int16_t m_max;
+    const int16_t m_min;
+    const float m_fine;
+    const float m_coarse;
+    const bool m_reversed;
+    volatile int16_t m_change;
+    volatile int16_t m_previous_state;
+    volatile int16_t m_steps;
 };
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,3 +1,25 @@
+/*
+Copyright 2023, Pontus Rydin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 #include <SPI.h>
 #include <MCP3202.h>
 #define USE_TIMER_3 true
@@ -22,28 +44,27 @@
 #define ADC_CS 23 // ADC chip select
 
 // ADC constants
-#define ADC_VREF 4.096
-#define ADC_MAX_VALUE 4096.0
-#define ADC_CURRENT 0
-#define ADC_VOLTAGE 1
+#define ADC_VREF 4.096       // ADC reference voltage
+#define ADC_MAX_VALUE 4096.0 // Maximum value returned from ADC (2^bits)
+#define ADC_CURRENT 0        // Curremt channel
+#define ADC_VOLTAGE 1        // Voltage channel
 
 // DAC constants
-#define DAC_VOLTAGE 1
-#define DAC_CURRENT 0
+#define DAC_VOLTAGE 1 // Voltage channel
+#define DAC_CURRENT 0 // Current channel
 
 // Timer interrupt
-#define ADC_AVG_INT 1000   // ADC averaging interval (ms)
-#define ADC_SAMPLE_INT 100 // ADC sampling interval (ms)
-#define MAX_SAMPLES (ADC_AVG_INT / ADC_SAMPLE_INT)
-#define POLL_ROTARY_INT 10 // Rotary switch polling interval (ms)
+#define ADC_AVG_INT 1000                           // ADC averaging interval (ms)
+#define ADC_SAMPLE_INT 100                         // ADC sampling interval (ms)
+#define MAX_SAMPLES (ADC_AVG_INT / ADC_SAMPLE_INT) // Number of samples to collect
 
-// Conversion factors
-#define MAX_VOLT 30.0
-#define MAX_AMP 2.0
-#define R_SENSE 0.5
-#define ADC_TO_RAW_VOLT(x) ((x * ADC_VREF) / ADC_MAX_VALUE)
-#define ADC_TO_VOLT(x) (ADC_TO_RAW_VOLT(x) * (MAX_VOLT / ADC_VREF))
-#define ADC_TO_AMP(x) (ADC_TO_RAW_VOLT(x) * (MAX_AMP / ADC_VREF))
+// Conversion factors and functions
+#define MAX_VOLT 30.0                                               // Maximum volts the supply can output
+#define MAX_AMP 2.0                                                 // Maximum amps the supply can output
+#define R_SENSE 0.5                                                 // Value of sense resistor (ohms)
+#define ADC_TO_RAW_VOLT(x) ((x * ADC_VREF) / ADC_MAX_VALUE)         // Convert ADC reading to actual volts seen on pin
+#define ADC_TO_VOLT(x) (ADC_TO_RAW_VOLT(x) * (MAX_VOLT / ADC_VREF)) // Convert ADC reading to volts on supply output
+#define ADC_TO_AMP(x) (ADC_TO_RAW_VOLT(x) * (MAX_AMP / ADC_VREF))   // Convert ADC reading to amps through load
 
 // Rotary encoders
 RotaryEncoder currentDial(ROTARY_DT_1, ROTARY_CLK_1, ROTARY_SW_1, 0, 200, 1, 10);
@@ -101,7 +122,6 @@ void setup()
   // Start timer interrupt
   ITimer3.init();
   ITimer3.attachInterruptInterval(ADC_SAMPLE_INT, onReadADC);
-  // ITimer3.attachInterruptInterval(POLL_ROTARY_INT, onPollRotary);
 
   // Set all DAC output voltages to zero
   dac.begin(DAC_CS);
@@ -113,13 +133,6 @@ void setup()
 
 void loop()
 {
-  // Read rotary encoders
-  // onReadADC();
-  // Serial.println(adc.readChannel(0));
-  // Serial.println(adc.readChannel(1));
-  // Serial.print(ADC_TO_RAW_VOLT(adc.readChannel(1)));
-  // Serial.print(" -> ");
-  // Serial.println(voltAvg);
   currentDial.service();
   voltageDial.service();
 
